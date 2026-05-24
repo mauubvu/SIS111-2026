e259ec2cd1bbfe8a0eee53eecac6a2a5df21595d
mauubvu <javierbobarin01@gmail.com>
2026-05-24 15:37:57 -0400

    24-05-26

--- TEMA3/python/prac5_1.py
+++ TEMA3/python/prac5_1.py
@@ -18,7 +18,7 @@ def generar(cantidad):
     return primos
 
 def indice(k):
-    PERM=[1,0,3,2,4]
+    PERM=[0,3,2,4,1]
     k-=1
     grupo=k//5
     pos_grupo=k%5
@@ -32,30 +32,9 @@ def indice_max(ns):
             max=idx
     return max
 
-def secuencia(ns,primos):
-    for n in ns:
-        print(primos[indice(n)])
-    print(primos)
-
 ns=[]
 for t in range(int(input())):
     ns.append(int(input()))
-print('---------')
 primos=generar(indice_max(ns)+1)
-for n in ns:
-    print(primos,indice(n))
-    print(primos[indice(n)])
-
-'''
-lista
-2,7,5,11,3
-13,23,19,29,17
-
-0 4 2 1
-
-primos
-2,3,5,7,11
-13,17,19,23,29
-
-out: 2,7,3,29
-'''
+for k in ns:
+    print(primos[indice(k)])
